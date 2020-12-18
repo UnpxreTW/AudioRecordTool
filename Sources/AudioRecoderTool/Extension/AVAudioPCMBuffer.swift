7e9d604e6ec995a932b3e1e973286799b0bc9231
Unpxre <unpxre.tw@gmail.com>
2020-12-18 17:07:18 +0800

    [add] AVAudioPCMBuffer 格式轉換擴展

---
+++ Sources/AudioRecoderTool/Extension/AVAudioPCMBuffer.swift
@@ -0,0 +1,33 @@
+//
+//  AVAudioPCMBuffer.swift
+//  
+//
+//  Created by UnpxreTW on 2020/11/17.
+//
+
+import AVFoundation
+
+extension AVAudioPCMBuffer {
+
+    /// 轉換為 Data 時的資料長度
+    /// - Note: 為 Frame 長度乘上每個 Frame 的 Bit 數。
+    private var dataLength: Int { Int(frameLength * format.streamDescription.pointee.mBytesPerFrame) }
+
+    /// 從 AVAudioPCMBuffer 取出 Int16 格式資料轉換為 Data。
+    var int16Data: Data? {
+        guard let pointer = UnsafeBufferPointer(start: int16ChannelData, count: 1).first else { return nil }
+        return Data(bytes: pointer, count: dataLength)
+    }
+
+    /// 從 AVAudioPCMBuffer 取出 Int32 格式資料轉換為 Data。
+    var int32Data: Data? {
+        guard let pointer = UnsafeBufferPointer(start: int32ChannelData, count: 1).first else { return nil }
+        return Data(bytes: pointer, count: dataLength)
+    }
+
+    /// 從 AVAudioPCMBuffer 取出 Float32 格式資料轉換為 Data。
+    var float32Data: Data? {
+        guard let pointer = UnsafeBufferPointer(start: floatChannelData, count: 1).first else { return nil }
+        return Data(bytes: pointer, count: dataLength)
+    }
+}
